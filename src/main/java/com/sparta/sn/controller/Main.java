d9a8f1382aae2f7d3fe5ceb00735f7d821639410
s-notta <53828792+s-notta@users.noreply.github.com>
2019-08-12 20:54:35 +0100

    Refactored and tested binary tree

--- src/main/java/com/sparta/sn/controller/Main.java
+++ src/main/java/com/sparta/sn/controller/Main.java
@@ -2,9 +2,6 @@ package com.sparta.sn.controller;
 
 import com.sparta.sn.exception.ChildNotFoundException;
 
-/**
- * Hello world!
- */
 public class Main {
     public static void main(String[] args) throws ChildNotFoundException {
         new BinarySearchManager().runBinaryTree();
